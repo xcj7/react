57ed66430acc39497414f6f254c35fc22f9178dd
xcj7 <xcjtushar@gmail.com>
2024-12-30 18:12:18 +0600

    megablog4

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 function Header() {
-  const authStatus = useSelector((state)=>state.auth.status)
+  const authStatus = useSelector((state)=> !!state.auth.status)
   const navigate = useNavigate()
   const navItems = [
     {
@@ -33,7 +33,10 @@ function Header() {
       slug:"/add-post",
       active:authStatus
     }
-  ]
+  ];
+  console.log("++++++++++++++++++", navItems);
+  navItems.forEach(item => console.log(typeof item.name, typeof item.slug));
+  console.log("authStatus:", typeof authStatus, authStatus);
 
   return (
 
@@ -48,7 +51,7 @@ function Header() {
             <ul className='flex ml-auto'>
               {navItems.map((item)=>
                 item.active ? (
-                  <li key={item.name}>
+                  <li key={item.slug}>
                     <button onClick={()=> navigate(item.slug)} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
                     >{item.name}</button>
                   </li>
--- MegaBlog/src/components/container/Container.jsx
+++ MegaBlog/src/components/container/Container.jsx
@@ -1,6 +1,6 @@
-import React, { Children } from 'react'
+import React  from 'react'
 
-function Container() {
+function Container({Children}) {
   return (
     <div className='w-full max-w-7xl mx-auto px=4'>
       {Children}
